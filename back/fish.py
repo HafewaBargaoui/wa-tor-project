3dcb0e4036af1b34c98c2edadc5f3bc65f0b73cf
seiyakazana <seiyakazana@gmail.com>
2025-05-06 15:51:34 +0200

    feat: :sparkles: class fish (#3)
    start of class fish
    
    Co-authored-by: elias <jean-pierre.elias@hotmail.fr>

---
+++ back/fish.py
@@ -0,0 +1,14 @@
+
+
+class Fish:
+    def __init__(self, pos_x: int, pos_y: int, repro_time: int):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.repro_time = repro_time
+
+    def move(self):
+        pass
+
+    def repro(self):
+        pass
+    
